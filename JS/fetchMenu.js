bcdc6f3986c72e2a5ed564e42335613c6750a67d
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-03-22 02:52:29 +0100

    Minor patches of css styles and html with br and border-radius, alse renamen Main html to Index html. Also added food description to json to get added into the Menu html through js.

--- JS/fetchMenu.js
+++ JS/fetchMenu.js
@@ -20,23 +20,34 @@ function fetchMenu() {
 
         data.appetizers.forEach(function(appetizer) {
             let li = document.createElement("li");
-            li.textContent = appetizer.Food_item + "...(Ingredienser kan påverka dessa allergier: " + appetizer.Food_allergens + ").....Alternativ Specialkost: "+ appetizer.Special_diet + "........Pris: " + appetizer.Food_price + "kr. ";
+            let hr = document.createElement("hr");
+            li.textContent = appetizer.Food_item +"("+ appetizer.Food_description +")...(Ingredienser kan påverka dessa allergier: " + appetizer.Food_allergens + ").....Alternativ Specialkost: "+ appetizer.Special_diet + "........Pris: " + appetizer.Food_price + "kr. ";
             li.setAttribute("class", appetizer.Food_type);
+            hr.setAttribute("class", appetizer.Food_type);
             appetizerList.appendChild(li);
+            appetizerList.appendChild(hr);
         });
 
         data.Maincourses.forEach(function(mainCourse) {
             let liTwo = document.createElement("li");
-            liTwo.textContent = mainCourse.Food_item + "...(Ingredienser kan påverka dessa allergier: " + mainCourse.Food_allergens + ").....Alternativ Specialkost: "+ mainCourse.Special_diet + "........Pris: " + mainCourse.Food_price + "kr. ";
+            let hrTwo = document.createElement("hr");
+            liTwo.textContent = mainCourse.Food_item +"("+ mainCourse.Food_description + ")...(Ingredienser kan påverka dessa allergier: " + mainCourse.Food_allergens + ").....Alternativ Specialkost: "+ mainCourse.Special_diet + "........Pris: " + mainCourse.Food_price + "kr. ";
             liTwo.setAttribute("class", mainCourse.Food_type);
+            hrTwo.setAttribute("class", mainCourse.Food_type);
             mainCourseList.appendChild(liTwo);
+            mainCourseList.appendChild(hrTwo);
+        
         });
 
         data.Desserts.forEach(function(dessert) {
             let liThree = document.createElement("li");
-            liThree.textContent = dessert.Food_item + "...(Ingredienser kan påverka dessa allergier: " + dessert.Food_allergens + ").....Alternativ Specialkost: "+ dessert.Special_diet + "........Pris: " + dessert.Food_price + ". ";
+            let hrThree = document.createElement("hr");
+            liThree.textContent = dessert.Food_item +"("+ dessert.Food_description + ")...(Ingredienser kan påverka dessa allergier: " + dessert.Food_allergens + ").....Alternativ Specialkost: "+ dessert.Special_diet + "........Pris: " + dessert.Food_price + ". ";
             liThree.setAttribute("class", dessert.Food_type);
+            hrThree.setAttribute("class", dessert.Food_type);
             dessertList.appendChild(liThree);
+            dessertList.appendChild(hrThree);
+            
         })
     })
     .catch(error =>{
---
+++ README.txt
@@ -0,0 +1,15 @@
+#Restaurang Grillos
+
+#Beskrivning
+Detta är min skolprojekt för Uppgift Interaktiv Webbsida
+
+#Struktur
+
+- `index.html`: Huvudsidan för Webbplatsen
+- `Menu.html`: Menyn sidan av Webbplatsen
+- `CSS/style.css`: Stilar båda sidorna för Webbplatsen
+- `JS/fetchMenu.js`: Hämtar Menynlistan från Json-Filen
+- `JS/Filtermenu.js`: Lägger till funktionaliten att filtrera katergorin i MenynListan
+- `JS/hideToggle.js`: Lägger till funktionalitet att dölja LunchBuffén containern
+- `JS/slideShow.js`: Lägger till funktionalitet av en slideshow för Huvudsidan av Webbplatsen
+ -`Json/Foodmenu.json`: Lägger till en data lagring av Menynlistan för att hämtas av fetchMenu funktion koden.
